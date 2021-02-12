acacc153c30c4b0d5db7eb28d8d9fecd463e887e
RicardFabien <ricard.fabienpro92@yahoo.fr>
2021-02-12 15:39:31 +0100

    Commit de milieu de projet

--- Evaluation/src/CalculSalaire/TestIntervenant.java
+++ Evaluation/src/CalculSalaire/TestIntervenant.java
@@ -1,4 +1,13 @@
 package CalculSalaire;
 
 public class TestIntervenant {
+    public static void main(String[] args) {
+
+        Salarie s1 = new Salarie("Pichon", "Jean", 2000);
+        System.out.println(s1.afficherDonnées());
+        Pigiste p1= new Pigiste("Poisson", "Jeanne", 15, 850);
+        System.out.println(p1.afficherDonnées());
+
+
+    }
 }
